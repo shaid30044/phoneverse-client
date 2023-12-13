814b5870f718bf4f2be05049b8ae6235ad872c87
shaid30044 <kaona30044@gmail.com>
2023-12-13 22:25:17 +0600

    implement react helmet async

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,13 @@
+import { Helmet } from "react-helmet-async";
 import Navbar from "../Shared/NavBar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Phone Verse</title>
+      </Helmet>
+
       <Navbar />
     </div>
   );
--- src/Pages/Phones.jsx
+++ src/Pages/Phones.jsx
@@ -1,8 +1,13 @@
+import { Helmet } from "react-helmet-async";
 import Navbar from "../Shared/NavBar/Navbar";
 
 const Phones = () => {
   return (
     <div>
+      <Helmet>
+        <title>Phone Verse | Phones</title>
+      </Helmet>
+
       <Navbar />
     </div>
   );
