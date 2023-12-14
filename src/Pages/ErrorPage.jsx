1a71d9e372226fc0ebe108332287229401a9ef46
shaid30044 <kaona30044@gmail.com>
2023-12-14 08:40:15 +0600

    modify Errro page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/phoneverse-logo.png";
-import phone from "../assets/nostalgie.webp";
+import phone from "../assets/pexels-markus-winkler-3639873.jpg";
 import Button from "../Shared/Button/Button";
 
 const ErrorPage = () => {
@@ -12,8 +12,8 @@ const ErrorPage = () => {
 
   return (
     <div className="bg-past h-screen p-4">
-      <div className="grid md:grid-cols-2 bg-white rounded-xl h-full">
-        <div className="relative p-6 md:pl-20 md:py-10">
+      <div className="grid lg:grid-cols-2 bg-white rounded-xl h-full">
+        <div className="relative p-6 md:px-20 md:py-10">
           <div className="w-20">
             <Link to="/">
               <img src={logo} alt="logo" className="w-20" />
@@ -26,14 +26,16 @@ const ErrorPage = () => {
 
               <h3 className="text-3xl font-bold pt-4">Page not found!</h3>
 
-              <p className="font-medium md:pr-10">
+              <p className="font-medium">
                 Oops... you may have mistype the address or the page may have
                 moved.
               </p>
             </div>
 
-            <div onClick={handleBackToHome}>
-              <Button type={"Back to Home"} />
+            <div>
+              <span onClick={handleBackToHome}>
+                <Button type={"Back to Home"} />
+              </span>
             </div>
           </div>
 
@@ -42,11 +44,11 @@ const ErrorPage = () => {
           </div>
         </div>
 
-        <div className="hidden md:block h-full">
+        <div className="relative hidden lg:block overflow-hidden bg-cover bg-no-repeat rounded-r-xl hover:rounded-r-xl">
           <img
             src={phone}
             alt="phone"
-            className="object-cover rounded-r-xl h-full"
+            className="transition-transform duration-1000 ease-in-out transform hover:-translate-y-20 xl:hover:-translate-y-60 rounded-r-xl"
           />
         </div>
       </div>
